5237bfde6a846fd63a01091eb4c8a81859c55bdd
cjohannessen <cjohannessen@us.ibm.com>
2017-01-26 14:50:58 -0600

    original commit

---
+++ ajaxGetItems.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'getItems.php';
+$result = RetrieveItems(http://localhost:6379/catalog/items");
+if (isset(json_decode($result)->errno)) {
+    http_response_code(500);
+}
+echo $result;
+
+?>
+
