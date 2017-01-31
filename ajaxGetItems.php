7d1c4a1ef4a29e259bf579f92b6d8fe3d479b6a8
wwdemo <cbjohann@us.ibm.com>
2017-01-31 22:37:26 +0100

    added missing quotes on loaditems

--- ajaxGetItems.php
+++ ajaxGetItems.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'getItems.php';
-$result = RetrieveItems(http://localhost:6379/catalog/items");
+$result = RetrieveItems("http://localhost:6379/catalog/items");
 if (isset(json_decode($result)->errno)) {
     http_response_code(500);
 }
